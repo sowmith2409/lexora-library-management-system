b9f03d91ce77702989648da3e8e3004f89400738
Sowmith <manthanasoumith@gmail.com>
2026-05-24 01:24:27 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // API INSTANCE
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://lexora-library-management-system.onrender.com/api",
 });
 
 // ADD TOKEN AUTOMATICALLY
